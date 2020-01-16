f56d8b30ab100eb78115b0dac34dc23a3e98500e
dimetriuss <lizogub.dmitriy@gmail.com>
2020-01-16 16:10:26 +0200

    Prime numbers search methods

---
+++ prime_numbers/prime_numbers.py
@@ -0,0 +1,30 @@
+def find_prime_numbers_from_range(upper_number: int) -> list:
+    if upper_number < 1:
+        raise ValueError("Please input upper_number >= 1")
+
+    primes = []
+    for num in range(2, upper_number+1):
+        for i in primes:
+            if num % i == 0:
+                break
+        else:
+            primes.append(num)
+    return primes
+
+
+def find_prime_numbers_from_range_with_eratosthenes_sieve(upper_number: int) -> list:
+    if upper_number < 1:
+        raise ValueError("Please input upper_number >= 1")
+
+    numbers_range = [x for x in range(upper_number + 1)]
+    numbers_range[1] = 0
+    primes = []
+
+    filtered_number = 2
+    while filtered_number <= upper_number:
+        if numbers_range[filtered_number] != 0:
+            primes.append(numbers_range[filtered_number])
+            for number in range(filtered_number, upper_number + 1, filtered_number):
+                numbers_range[number] = 0
+        filtered_number += 1
+    return primes
