37da595a67587e04a3ad31579ba96b79598c0994
dimetriuss <lizogub.dmitriy@gmail.com>
2020-01-27 16:47:09 +0200

    Add method to find fibonacci sequence between 2 values

--- fibonacci_sequence/fibonacci.py
+++ fibonacci_sequence/fibonacci.py
@@ -30,3 +30,28 @@ def find_fibonacci_number_golden_ratio(number_index: int) -> int:
         return 1
     golden_ratio = (1 + 5 ** 0.5) / 2
     return int((golden_ratio ** (number_index + 1) + 1) / 5 ** 0.5)
+
+
+def find_fibonacci_numbers_in_values_range(low: int, high: int) -> list:
+    if low < 0:
+        raise ValueError("Please input low >= 0")
+    if high < 1 or low == high:
+        raise ValueError("Please input high > low >= 0")
+
+    fib_numbers = []
+    previous_number1 = 0
+    previous_number2 = 1
+    if previous_number1 >= low:
+        fib_numbers.append(previous_number1)
+    if previous_number2 >= low:
+        fib_numbers.append(previous_number2)
+
+    while True:
+        current_number = previous_number1 + previous_number2
+        if current_number > high:
+            break
+        if current_number >= low:
+            fib_numbers.append(current_number)
+        previous_number1, previous_number2 = previous_number2, current_number
+
+    return fib_numbers
